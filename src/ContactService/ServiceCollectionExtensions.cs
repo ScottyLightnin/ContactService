2d9de1f89eaa4a02e69fe2ea3ad2efa7367ddd6f
Scott Ferguson <lightninnzl@gmail.com>
2020-10-02 22:18:41 +1300

    Added AddContactService ServiceCollectionExtensions

---
+++ src/ContactService/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace LightningRodInteractive.ContactService
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContactService(this IServiceCollection services, Action<ContactServiceOptions> configureOptions)
+        {
+            services.Configure(configureOptions);
+            services.AddHttpClient(nameof(ContactService));
+            services.AddSingleton(sp => sp.GetRequiredService<IOptions<ContactServiceOptions>>().Value);
+            services.AddTransient<IContactService, ContactService>();
+            return services;
+        }
+    }
+}
